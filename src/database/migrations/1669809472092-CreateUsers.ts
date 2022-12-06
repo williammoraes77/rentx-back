571e67b16ab7fc79b15db2bd1b78d6a73eca2be4
William Moraes <willmoraes77@gmail.com>
2022-12-06 15:53:32 -0300

    add authenticate

--- src/database/migrations/1669809472092-CreateUsers.ts
+++ src/database/migrations/1669809472092-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1669809472092 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
